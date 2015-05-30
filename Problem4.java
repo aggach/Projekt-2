f28203324412fa28426fd27bea040232a8bba288
agnieszka.chmielowiec <aggach064@gmail.com>
2015-05-30 21:59:56 +0200

    Dodanie drugie testu, blad test.

--- Problem4.java
+++ Problem4.java
@@ -9,6 +9,9 @@ public class Problem4 {
 		}
 		return true;
 	}
+	
+	public static int greatestPalindrom(int max) {
+	}
 
 
 	public static void main(String[] args) {
--- Problem4Test.java
+++ Problem4Test.java
@@ -10,4 +10,11 @@ public class Problem4Test extends TestCase {
 		assertTrue(Problem4.isPal(9009));
 		assertFalse(Problem4.isPal(9010));
 	}
+	
+
+	@Test
+	public void testgreatestPalindrom() {
+		assertEquals(9009, Problem4.greatestPalindrom(99));
+	}
+
 }

4a77d86e512b118627f57d046b894da5fc5cff5b
agnieszka.chmielowiec <aggach064@gmail.com>
2015-05-30 21:53:45 +0200

    Pierwszy test przeszedl z powodzeniem

--- Problem4.java
+++ Problem4.java
@@ -2,7 +2,14 @@
 public class Problem4 {
 
 	public static boolean isPal(int number) {
-}
+		String s = new Integer(number).toString();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != s.charAt(s.length() - 1 - i))
+				return false;
+		}
+		return true;
+	}
+
 
 	public static void main(String[] args) {
 	}
